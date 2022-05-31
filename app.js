5a5ab4c46520b6fdea67a9d132ae8b74d19b8fc9
Temuujin2 <103709595+Temkasobri@users.noreply.github.com>
2022-05-31 11:38:18 +0800

    May 31 update
    Zarim nemelt uurchlultvvd

--- app.js
+++ app.js
@@ -27,19 +27,20 @@ window.onclick = function(event) {
 
 // ==================background mouse move===================
 document.addEventListener("mouseover", parallax);
-function parallax(e){
+
+function parallax(e) {
     this.querySelectorAll(".layer").forEach(layer => {
         const speed = layer.getAttribute('data-speed')
 
-        const x = (window.innerWidth - e.pageX*speed)/1000
-        const y = (window.innerHeight - e.pageY*speed)/1000
+        const x = (window.innerWidth - e.pageX * speed) / 1000
+        const y = (window.innerHeight - e.pageY * speed) / 1000
 
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
     })
 }
 
-$("figure").mouseleave(
-    function() {
-      $(this).removeClass("hover");
-    }
-  );
+// $("figure").mouseleave(
+//     function() {
+//         $(this).removeClass("hover");
+//     }
+// );
